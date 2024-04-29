04b787f1b86d45fc436f5df30860963592775cbd
Aminul Islam <aminultension@gmail.com>
2024-04-30 01:42:55 +0530

    Service logic write

--- client-side/src/pages/Service.jsx
+++ client-side/src/pages/Service.jsx
@@ -1,48 +1,56 @@
 import React from "react";
 import { useAuth } from "../store/auth";
+import img from "../image/img1.png";
+import { useState } from "react";
 
 const Service = () => {
   const { service } = useAuth();
-  const fun = () => {
-    console.log("The service is : ", service);
-  }
+
+  const [count, setCount] = useState(0);
+
+  const data = service.response;
+
+  const cardStyle = {
+    border: "1px solid red",
+    padding: "10px",
+    margin: "10px",
+    width: "225px",
+    height: "400px",
+
+    display: "inline-block",
+  };
+
+  const imgStyle = {
+    width: "200px",
+    height: "200px",
+  };
+
+  const contentStyle = {
+    display: "flex",
+    justifyContent: "center",
+    flexWrap: "wrap",
+  };
+
   return (
     <div className="service-container">
       <h1 className="heading">Our Services</h1>
-      <p className="paragraph">Aminul</p>
-      <button onClick={fun}>Click</button>
       {/* map to print all the data card */}
-      
-      
-      <div className="service-card">
-        <div className="card">
-          <img src="https://placekitten.com/200/200" alt="Service 1" />
-          <h2>Service 1</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget
-            felis non nisi scelerisque euismod ut et sapien.
-          </p>
-        </div>
-
-        <div className="card">
-          <img src="https://placekitten.com/200/200" alt="Service 2" />
-          <h2>Service 2</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget
-            felis non nisi scelerisque euismod ut et sapien.
-          </p>
-        </div>
-
-        <div className="card">
-          <img src="https://placekitten.com/200/200" alt="Service 3" />
-          <h2>Service 3</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget
-            felis non nisi scelerisque euismod ut et sapien.
-          </p>
-        </div>
+      <div className="content" style={contentStyle}>
+        {data &&
+          data.map((item) => (
+            <div key={item.id} style={cardStyle}>
+              <img style={imgStyle} src={img} alt="Service 1" />
+              <h2>{item.service_name}</h2>
+              <p>{item.description}</p>
+              <h3>{item.price}</h3>
+              <p>{item.provider}</p>
+            </div>
+          ))}
       </div>
-    </div>
+
+      {/* Hardcoded data */}
+
+      </div>
   );
 };
 
