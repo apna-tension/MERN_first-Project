19e801c5bd1185102d11dc5724775fe8035baf99
Aminul Islam <aminultension@gmail.com>
2024-05-03 00:15:59 +0530

    fix the bug on error middleware

--- firstMern/middlewares/error-middlewares.js
+++ firstMern/middlewares/error-middlewares.js
@@ -3,6 +3,7 @@ const errorMiddleware = (error, req, res, next) => {
   const errorMessage = error.message || "Backend error";
   const status = error.status || 500;
   const extraDetails = error.extraDetails || null;
+  console.log("Hello Error");
   res.status(status).json({ message: errorMessage, extraDetails });
 };
 
