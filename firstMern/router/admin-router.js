acad672bc79e57ca29aff7477a9072fb4e697235
Aminul Islam <aminultension@gmail.com>
2024-05-06 02:23:17 +0530

    add admin middleware in admin pages

--- firstMern/router/admin-router.js
+++ firstMern/router/admin-router.js
@@ -1,9 +1,15 @@
-const express = require('express');
-const adminControllers = require('../controllers/admin-controller');
+const express = require("express");
+const adminControllers = require("../controllers/admin-controller");
 const router = express.Router();
 
-router.route("/users").get(adminControllers.getAllUsers);
-router.route("/contacts").get(adminControllers.getAllContacts);
+const authMiddleware = require("../middlewares/auth-middleware");
+const adminMiddleware = require("../middlewares/admin-middleware");
 
+router
+  .route("/users")
+  .get(authMiddleware, adminMiddleware, adminControllers.getAllUsers);
+router
+  .route("/contacts")
+  .get(authMiddleware, adminMiddleware, adminControllers.getAllContacts);
 
 module.exports = router;
