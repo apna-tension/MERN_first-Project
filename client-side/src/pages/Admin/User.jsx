44e17d2661069d8aa60ede3d878ff2f52fa0ebf8
Aminul Islam <aminultension@gmail.com>
2024-05-06 02:22:17 +0530

    add admin verification middleware

--- client-side/src/pages/Admin/User.jsx
+++ client-side/src/pages/Admin/User.jsx
@@ -1,40 +1,43 @@
 import React from 'react';
+
+import '../../style/AdminStyle/User.css'; // Import CSS file for styling
+
 import { useAuth } from '../../store/auth';
 
 const User = () => {
     const { adminUser } = useAuth();
-    console.log("adminUser >> ", adminUser);
+    console.log("my user is : ", adminUser);
+    return (
+        <div className="user-page">
+            <h2>User</h2>
 
-  return (
-    <>
-        <div>User</div>
-
-        {/* make a table to show data */}
-        <table className="table table-hover" border={2}>
-            <thead>
-                <tr>
-                    <th scope="col">UserName</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">Phone</th>
-                    <th scope="col">Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                {adminUser && adminUser.map((user, index) => (
-                    <tr key={index}>
-                        <td>{user.username}</td>
-                        <td>{user.email}</td>
-                        <td>{user.phone}</td>
-                        <td>
-                            <button className="btn btn-warning">Edit</button>
-                            <button className="btn btn-danger">Delete</button>
-                        </td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
-    </>
-  )
+            <div className="user-table-container">
+                <table className="user-table">
+                    <thead>
+                        <tr>
+                            <th>UserName</th>
+                            <th>Email</th>
+                            <th>Phone</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {adminUser && adminUser.map((user, index) => (
+                            <tr key={index}>
+                                <td>{user.username}</td>
+                                <td>{user.email}</td>
+                                <td>{user.phone}</td>
+                                <td>
+                                    <button className="edit-btn">Edit</button>
+                                    <button className="delete-btn">Delete</button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
 }
 
-export default User
+export default User;
